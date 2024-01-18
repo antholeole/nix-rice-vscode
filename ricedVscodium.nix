0133885c900ed8e08fbed17f67e0945e4b502fd2
oleina <oleina@google.com>
2024-01-17 22:20:06 -0800

    not workin

--- ricedVscodium.nix
+++ ricedVscodium.nix
@@ -1,30 +1,39 @@
 { 
     pkgs, 
     pkg,
-    rice ? { 
-        css = []; 
-        js = [];
-    }, 
-    ... 
+    css ? [],
+    js ? [],
+    ...
 }: let
 	lib = pkgs.lib;
 
     htmlPath = "resources/app/out/vs/code/electron-sandbox/workbench/workbench.html";
 
+    files = with builtins; let 
+        assetsPath = "TODO";
+
+        mapPath = fileType: files: map (old: {
+            inherit old;
+            new = "${assetsPath}/${builins.hashFile "sha256" old}.${fileType}";
+        }) files;
+    in {
+        css = mapPath "css" css;
+        js = mapPath "js" js;
+    };
+
+    # We have to put the files in one of the directories under /gulpfile.vscode.js#L210
+
     mkCss = path: "<link rel=\"stylesheet\" type=\"text/css\" href=\"${path}\">";
     mkJs = path: "<script type=\"text/javascript\" src=\"${path}\"></script>";
-
-    
-
 in pkg.overrideAttrs {
-    postInstall = ''
-    cp ${}
-    '';
+    postInstall = let 
+        allFiles = files.css ++ files.js;
+        cpCmds = builtins.map (file: "cp ${file.old} ${file.new}") allFiles;        
+
+    in lib.strings.concatStringsSep "\n" cpCmds;
 
     patches = let 
-
-    
-        rawLines = (map mkCss rice.css or []) ++ (map mkJs rice.js or []);
+        rawLines = (map mkCss files.css.new) ++ (map mkJs files.js.new);
         patchLines = map (rawLine: "+	${rawLine}") rawLines; 
 
         patch = with builtins; ''
