e1d069b984f502dd3d802bb486378b7f756b1ce6
oleina <oleina@google.com>
2024-12-11 16:40:02 -0800

    some changes

--- flake.nix
+++ flake.nix
@@ -1,25 +1,32 @@
 {
   description = "A patched version of vscodium with custom CSS and JS.";
-  inputs.systems.url = "github:nix-systems/default";
 
-  outputs = { self, systems, nixpkgs }:
-    let
-      eachSystem = nixpkgs.lib.genAttrs (import systems);
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    systems.url = "github:nix-systems/default";
+  };
 
-      ricedVscodiumBuilder = import ./ricedVscodium.nix;
-    in
-    {
-      packages = eachSystem (system: let 
-        pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        ricedVscodium = settings: (ricedVscodiumBuilder ({
+  outputs = {
+    self,
+    systems,
+    nixpkgs,
+  }: let
+    eachSystem = nixpkgs.lib.genAttrs (import systems);
+
+    ricedVscodiumBuilder = import ./ricedVscodium.nix;
+  in {
+    packages = eachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      ricedVscodium = settings: (ricedVscodiumBuilder ({
           inherit pkgs;
-        } // settings));
+        }
+        // settings));
 
-        testRicedVscodium = ricedVscodium {
-          css = [./test.css];
-          pkg = pkgs.vscodium;
-        };
-      });
-    };
+      testRicedVscodium = ricedVscodium {
+        css = [./test.css];
+        pkg = pkgs.vscodium;
+      };
+    });
+  };
 }
--- ricedVscodium.nix
+++ ricedVscodium.nix
@@ -1,55 +1,59 @@
-{ 
-    pkgs, 
-    pkg,
-    css ? [],
-    js ? [],
-    ... 
+{
+  pkgs,
+  pkg,
+  css ? [],
+  js ? [],
+  ...
 }: let
-	lib = pkgs.lib;
+  lib = pkgs.lib;
 
-    htmlPath = "resources/app/out/vs/code/electron-sandbox/workbench/workbench.html";
-    assetsPath = "lib/vscode/resources/app/out/vs/code/electron-sandbox/workbench";
+  htmlPath = "resources/app/out/vs/code/electron-sandbox/workbench/workbench.html";
+  assetsPath = "lib/vscode/resources/app/out/vs/code/electron-sandbox/workbench";
 
-    files = with builtins; let 
-        mapPath = fileType: files: map (old: {
-            inherit old;
-            new = "${builtins.hashFile "sha256" old}.${fileType}";
-        }) files;
-    in {
-        css = mapPath "css" css;
-        js = mapPath "js" js;
-    };
-in pkg.overrideAttrs {
-    postInstall = let 
-        allFiles = files.css ++ files.js;
-        cpCmds = builtins.map (file: "cp ${file.old} $out/${assetsPath}/${file.new}") allFiles;        
+  files = with builtins; let
+    mapPath = fileType: files:
+      map (old: {
+        inherit old;
+        new = "${builtins.hashFile "sha256" old}.${fileType}";
+      })
+      files;
+  in {
+    css = mapPath "css" css;
+    js = mapPath "js" js;
+  };
+in
+  pkg.overrideAttrs {
+    postInstall = let
+      allFiles = files.css ++ files.js;
+      cpCmds = builtins.map (file: "cp ${file.old} $out/${assetsPath}/${file.new}") allFiles;
+    in
+      lib.strings.concatStringsSep "\n" cpCmds;
 
-    in lib.strings.concatStringsSep "\n" cpCmds;
+    patches = with builtins; let
+      mkCss = path: "<link rel=\"stylesheet\" type=\"text/css\" href=\"${path.new}\">";
+      mkJs = path: "<script type=\"text/javascript\" src=\"${path.new}\"></script>";
 
-    patches = with builtins; let 
-        mkCss = path: "<link rel=\"stylesheet\" type=\"text/css\" href=\"${path.new}\">";
-        mkJs = path: "<script type=\"text/javascript\" src=\"${path.new}\"></script>";
+      rawLines = (map mkCss files.css) ++ (map mkJs files.js);
+      patchLines = map (rawLine: "+	${rawLine}") rawLines;
 
-        rawLines = (map mkCss files.css) ++ (map mkJs files.js);
-        patchLines = map (rawLine: "+	${rawLine}") rawLines; 
-
-        patch =''
+      patch = ''
 diff --git a/${htmlPath} b/${htmlPath}
 index eb525bd..e0d57bf 100644
 --- a/${htmlPath}
 +++ b/${htmlPath}
-@@ -62,6 +62,${toString (6 + length patchLines)} @@
- 					tokenizeToString
- 				;
- 		"/>
-${lib.strings.concatStringsSep "\n" patchLines}
- 	</head>
+@@ -66,6 +66,${toString (6 + length patchLines)} @@
  
- 	<body aria-label="">
-        '';
+${lib.strings.concatStringsSep "\n" patchLines}
+ 		<!-- Workbench CSS -->
+ 		<link rel="stylesheet" href="../../../workbench/workbench.desktop.main.css">
+ 	</head>
+      '';
 
       patchFile = pkgs.writeText "injected.patch" patch;
-    in if (builtins.length patchLines > 0) then [
+    in
+      if (builtins.length patchLines > 0)
+      then [
         patchFile
-    ] else [];
-}
+      ]
+      else [];
+  }
